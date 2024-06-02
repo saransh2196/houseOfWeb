bc967cf109fe8f25a27d4dd9a5b0050605a91745
saransh <saransh2196@gmail.com>
2024-06-02 17:01:05 +0530

    made docker containerisation

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,20 @@ import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import userRoutes from "./routes/user.routes";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = process.env.DB_NAME;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/user-management");
+const mongoURI = `${MONGODB_URI}/${DB_NAME}`;
+mongoose.connect(mongoURI);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
